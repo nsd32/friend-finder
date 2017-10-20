9ff02f046707da28a32e39dd35d0bc7e2dbb3aaf
Lea Abeyta <lea.abeyta@gmail.com>
2017-10-19 22:56:31 -0600

    fixed name display

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,31 +17,33 @@ module.exports = function(app) {
 		console.log(req.body.scores[0]);
 		var newFriend = req.body; 
 		var index = 0;
-    var smallestTotal = 0;
+		var smallestTotal = 0;
 
-    for (let k = 0; k < friends[0].scores.length; k++) {
-      smallestTotal += (Math.abs(newFriend.scores[k] - friends[0].scores[k]));
-    }
+		for (let k = 0; k < friends[0].scores.length; k++) {
+		  smallestTotal += (Math.abs(newFriend.scores[k] - friends[0].scores[k]));
+		}
 
-    for (let i = 1; i < friends.length; i++) {
-      var currentTotal = 0;
+		for (let i = 1; i < friends.length; i++) {
+		  var currentTotal = 0;
 
-      for (let j = 0; j < friends[i].scores.length; j++) { 
-        currentTotal += (Math.abs(friends[i].scores[j] - newFriend.scores[j]));
-      }
+	            for (let j = 0; j < friends[i].scores.length; j++) { 
+		      currentTotal += (Math.abs(friends[i].scores[j] - newFriend.scores[j]));
+		    }
 
-      if (currentTotal < smallestTotal) {
-        smallestTotal = currentTotal;
-        index = i;
-      }
-    }
-    console.log('Smallest Total: ' + smallestTotal);
-    console.log('Index: ' + index);
-    console.log(friends[index]); 
+		    if (currentTotal < smallestTotal) {
+		      smallestTotal = currentTotal;
+		      index = i;
+		    }
+	        }
+		
+	        console.log('Smallest Total: ' + smallestTotal);
+	        console.log('Index: ' + index);
+	        console.log(friends[index]); 
 
-    res.send(friends[index]);
-	  
-	  friends.push(newFriend);
-  });
+	        res.send(friends[index]);
+
+	        friends.push(newFriend);
+		
+	});
 
 }
