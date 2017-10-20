1053378a7ae60ef971c7877d11e1757ffd859c90
Lea Abeyta <lea.abeyta@gmail.com>
2017-10-19 22:20:31 -0600

    added characters and cleaned up ui and indentation

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,86 @@ var friends = [
 	      4,
 	      1
 	    ]
+	},
+	{
+	  "name":"Jimmy Fallon",
+	  "photo":"http://images.nymag.com/arts/tv/profiles/fallon101115_2_250.jpg",
+	  "scores":[
+	      2,
+	      4,
+	      1,
+	      5,
+	      2,
+	      4,
+	      3,
+	      3,
+	      2,
+	      4
+	    ]
+	},
+	{
+	  "name":"Jerry Seinfeld",
+	  "photo":"https://static.stereogum.com/uploads/2010/01/jerry_seinfeld.jpg",
+	  "scores":[
+	      1,
+	      2,
+	      3,
+	      4,
+	      5,
+	      1,
+	      2,
+	      3,
+	      4,
+	      5
+	    ]
+	},
+	{
+	  "name":"Michael Jordan",
+	  "photo":"https://i.pinimg.com/736x/d6/ff/f5/d6fff582994bd0815e22ebfedbc8e16e--jordan--michael-jordan.jpg",
+	  "scores":[
+	      5,
+	      4,
+	      3,
+	      2,
+	      1,
+	      5,
+	      4,
+	      3,
+	      2,
+	      1
+	    ]
+	},
+	{
+	  "name":"Ellen Degeneres",
+	  "photo":"http://ghk.h-cdn.co/assets/17/32/980x490/landscape-1502298247-ellen-degeneres-good-housekeeping-cover-3.jpg",
+	  "scores":[
+	      2,
+	      2,
+	      3,
+	      2,
+	      2,
+	      3,
+	      3,
+	      3,
+	      3,
+	      2
+	    ]
+	},
+	{
+	  "name":"Oprah Winfrey",
+	  "photo":"https://s.yimg.com/ny/api/res/1.2/5G1bFMcFo.AaoLv4hy7_Bg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/video/wsym_lansing_scripps_6/2740c3b5e416ea301a43652852ba19c1",
+	  "scores":[
+	      5,
+	      5,
+	      5,
+	      1,
+	      1,
+	      1,
+	      3,
+	      3,
+	      3,
+	      2
+	    ]
 	}
 
 ]
